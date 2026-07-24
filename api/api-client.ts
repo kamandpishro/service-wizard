52b0166540c72d54c02bb4ee348bdb0354259c63
kamand pishro <kamandpishro1379@gmail.com>
2026-07-24 19:36:59 +0330

    handle backendMessage in api client file

--- api/api-client.ts
+++ api/api-client.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
+import IApiErrorResponse from './type';
 
 export const apiClient = axios.create({
   baseURL: 'https://run.mocky.io/v3',
@@ -13,7 +14,7 @@ apiClient.interceptors.request.use(
   (config) => {
     const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
     if (token && config.headers) {
-      config.headers.Authorization = `Bearer${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
@@ -24,22 +25,28 @@ apiClient.interceptors.request.use(
 
 apiClient.interceptors.response.use(
   (response) => response,
-  (error) => {
-    if (error.response) {
-      const status = error?.response?.status;
 
-      if (status === 401) {
-        if (typeof window !== 'undefined') {
-          localStorage.removeItem('token');
-          window.location.href = '/login';
-        }
-      } else if (status === 403) {
-        toast.error('شما دسترسی لازم برای این عملیات را ندارید.');
-      } else if (status >= 500) {
-        toast.error('خطایی در سمت سرور رخ داده است. لطفاً بعداً تلاش کنید.');
-      } else {
-        toast.error('خطا در اتصال به شبکه. لطفا وضعیت اینترنت خود را بررسی کنید.');
+  (error) => {
+    if (!error.response) {
+      toast.error('خطا در اتصال به شبکه. لطفاً وضعیت اینترنت خود را بررسی کنید.');
+      return Promise.reject(error);
+    }
+    const status = error.response.status;
+    const data = error.response.data as IApiErrorResponse | null;
+    const backendMessage = data?.message;
+
+    if (status === 401) {
+      if (typeof window !== 'undefined') {
+        toast.error(backendMessage || 'لطفا مجددا وارد شوید.');
+        localStorage.removeItem('token');
+        window.location.href = '/login';
       }
+    } else if (status === 403) {
+      toast.error(backendMessage || 'شما دسترسی لازم برای این عملیات را ندارید.');
+    } else if (status >= 500) {
+      toast.error(backendMessage || 'خطایی در سمت سرور رخ داده است. لطفاً بعداً تلاش کنید.');
+    } else {
+      toast.error(backendMessage || 'درخواست با خطا مواجه شد.');
     }
     return Promise.reject(error);
   },
